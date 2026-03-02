c73230e73ee554000a3b27c7696f248d78fc434b
jarquingijonomarantonio69-byte <jarquingijonomarantonio69@gmail.com>
2026-03-02 13:35:00 -0600

    creacion de listado en sidenav

--- src/app/modules/shared/components/sidenav/sidenav.ts
+++ src/app/modules/shared/components/sidenav/sidenav.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule} from '../../material-module';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { MediaMatcher } from '@angular/cdk/layout';
 
 
 @Component({
   selector: 'app-sidenav',
-  imports: [CommonModule, MaterialModule, RouterOutlet],
+  imports: [CommonModule, MaterialModule, RouterOutlet, RouterLinkWithHref],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.css',
 })
 export class Sidenav {
+  menuNav = [
+    {name: 'Home', route: 'home', icon: 'home'},
+    {name: 'Categorias', route: 'category', icon: 'category'},
+    {name: 'productos', route: 'product', icon: 'production_quantity_limits'},
+  ];
 
   mobileQuery: MediaQueryList;
+menuItems: any;
   constructor(media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
   }
